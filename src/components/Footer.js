aae6267333e959dd4883c94eb9dc67a9bf439fb3
MMartinJA <moschinimartin8@gmail.com>
2023-11-06 12:03:49 -0300

    clase reac arrancamos citio

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = (props) => {
+    return (
+        <footer>
+            <p>Diseñado por: Moschini Martin</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = (props) => {
+    return (
+        <header>
+            <h1>Trabajamos con React</h1>
+        </header>
+    )
+}
+
+export default Header;
